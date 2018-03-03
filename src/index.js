81b79e5f22971760041e206fdc80aa70dfb7a8c2
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-03-03 01:43:50 +0100

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import 'normalize.css';
 import 'react-flexbox-grid';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
